68affce854ac6bdcf0dec0074af19b19bbd0a134
jamesfowler <JamesFowlerTX@yahoo.com>
2018-04-10 11:26:26 -0500

    added error messages for register Servlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
@@ -19,19 +20,57 @@ public class RegisterServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
+        String first_name = request.getParameter("first_name");
+        String last_name = request.getParameter("last_name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
         // validate input
-        boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
+//        boolean inputHasErrors = username.isEmpty()
+//            || email.isEmpty()
+//            || password.isEmpty()
+//            || (! password.equals(passwordConfirmation));
 
-        if (inputHasErrors) {
-            response.sendRedirect("/register");
-            return;
+//        if (inputHasErrors) {
+//            response.sendRedirect("/register");
+//            return;
+//        }
+
+        boolean usernameError = username.isEmpty();
+        boolean firstNameError = first_name.isEmpty();
+        boolean lastNameError = last_name.isEmpty();
+        boolean emailError = email.isEmpty();
+        boolean passwordError = password.isEmpty();
+        boolean passwordConfirmationError = ! password.equals(password);
+
+
+        if (usernameError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Username");
+        }
+
+        if (firstNameError) {
+            JOptionPane.showMessageDialog(null, "Please enter a First Name");
+        }
+        if (lastNameError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Last Name");
+        }
+
+        if (emailError) {
+            JOptionPane.showMessageDialog(null, "Please enter an Email");
+        }
+
+        if (passwordError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Passoword");
+        }
+
+        if (passwordConfirmationError) {
+            JOptionPane.showMessageDialog(null, "Please confirm your Password");
+        }
+
+
+        if(username.equals(username)) {
+            JOptionPane.showMessageDialog(null, "This username is already taken, please choose another.");
         }
 
         // create and save a new user
