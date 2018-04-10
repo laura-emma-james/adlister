61a3663283d3c21e7b144624a6d30069d786b752
jamesfowler <JamesFowlerTX@yahoo.com>
2018-04-10 13:40:35 -0500

    error messsages fixed

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -22,21 +22,23 @@ public class RegisterServlet extends HttpServlet {
         String first_name = request.getParameter("first_name");
         String last_name = request.getParameter("last_name");
         String username = request.getParameter("username");
-        String first_name = request.getParameter("first_name");
-        String last_name = request.getParameter("last_name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
         // validate input
 //        boolean inputHasErrors = username.isEmpty()
+//            || first_name.isEmpty()
+//            || last_name.isEmpty()
 //            || email.isEmpty()
-//            || password.isEmpty()
-//            || (! password.equals(passwordConfirmation));
-
+//            || password.isEmpty();
+//        boolean passwordConfirmationError = ! password.equals(password);
+//
 //        if (inputHasErrors) {
-//            response.sendRedirect("/register");
-//            return;
+//            JOptionPane.showMessageDialog(null, "Check to see if you have entered all of the required feilds.");
+//        }
+//        else if(passwordConfirmationError) {
+//            JOptionPane.showMessageDialog(null, "Please confirm your Password");
 //        }
 
         boolean usernameError = username.isEmpty();
@@ -45,33 +47,34 @@ public class RegisterServlet extends HttpServlet {
         boolean emailError = email.isEmpty();
         boolean passwordError = password.isEmpty();
         boolean passwordConfirmationError = ! password.equals(password);
-
-
         if (usernameError) {
-            JOptionPane.showMessageDialog(null, "Please enter a Username");
+            JOptionPane.showMessageDialog(null, "Please enter a Username.");
         }
-
+        if(usernameError && firstNameError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Username and First Name.");
+        }
+        if(usernameError && lastNameError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Username and Last Name.");
+        }
+        if(usernameError && emailError) {
+            JOptionPane.showMessageDialog(null, "Please enter a Username and Email.");
+        }
         if (firstNameError) {
             JOptionPane.showMessageDialog(null, "Please enter a First Name");
         }
         if (lastNameError) {
             JOptionPane.showMessageDialog(null, "Please enter a Last Name");
         }
-
         if (emailError) {
             JOptionPane.showMessageDialog(null, "Please enter an Email");
         }
-
         if (passwordError) {
             JOptionPane.showMessageDialog(null, "Please enter a Passoword");
         }
-
         if (passwordConfirmationError) {
             JOptionPane.showMessageDialog(null, "Please confirm your Password");
         }
-
-
-        if(username.equals(username)) {
+        if(username.equals()) {
             JOptionPane.showMessageDialog(null, "This username is already taken, please choose another.");
         }
 
@@ -82,4 +85,3 @@ public class RegisterServlet extends HttpServlet {
         response.sendRedirect("/login");
     }
 }
-y
