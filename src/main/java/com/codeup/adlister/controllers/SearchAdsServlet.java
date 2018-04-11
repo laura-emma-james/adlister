d356975869efcc9efe8fe2d1fa2848a926e6c05c
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-11 06:40:28 -0500

    save method for searching ads

--- src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,18 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "SearchAdsServlet")
+@WebServlet(name = "SearchAdsServlet", urlPatterns = "/searchAds")
 public class SearchAdsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setAttribute("ads", DaoFactory.getAdsDao().searchAds());
+        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
 }
-//TODO:Add search method to MySQLAdsDao.java File
-//    public List<Ad> searchAllAds(title) {
+//TODO:Add search method to MySQLAdsDao.java File and call method from the Ads.java file both searching sellers ad's profile and ads index
+//TODO: Will reference this method for searching that particular JSP.
+//    public List<Ad> searchAds() {
 //        PreparedStatement stmt = null;
 //        try {
 //            stmt = connection.prepareStatement("SELECT * FROM ads WHERE title = ?");
