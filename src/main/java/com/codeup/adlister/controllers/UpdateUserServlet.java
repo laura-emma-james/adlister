24b8383603ed077f3766f6676b80909e1c1c0329
emmadejong <dejong.l.emma@gmail.com>
2018-04-11 16:24:58 -0500

    added dopost methods for finsihing update user functions

--- src/main/java/com/codeup/adlister/controllers/UpdateUserServlet.java
+++ src/main/java/com/codeup/adlister/controllers/UpdateUserServlet.java
@@ -1,21 +1,44 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.Users;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "UpdateUserServlet", urlPatterns = "/profile/update")
 public class UpdateUserServlet extends HttpServlet{
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/updateUser.jsp").forward(req, resp);
     }
 
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));
 
+        String first_name = req.getParameter("first_name");
+        String last_name = req.getParameter("last_name");
+        String bio = req.getParameter("bio");
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = DaoFactory.getUsersDao().findByUserId(id);
+
+        user.setId(id);
+        user.setFirst_name(first_name);
+        user.setLast_name(last_name);
+        user.setBio(bio);
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        DaoFactory.getUsersDao().update(user);
+        req.getSession().setAttribute("user", user);
+        resp.sendRedirect("/profile");
     }
 }
