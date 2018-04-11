ff0afec47449542cdd032bfe34cc4d6c741ed506
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-11 06:27:59 -0500

    working on Search servlet and method for searching

---
+++ src/main/java/com/codeup/adlister/controllers/SearchAdsServlet.java
@@ -0,0 +1,30 @@
+package com.codeup.adlister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SearchAdsServlet")
+public class SearchAdsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+//TODO:Add search method to MySQLAdsDao.java File
+//    public List<Ad> searchAllAds(title) {
+//        PreparedStatement stmt = null;
+//        try {
+//            stmt = connection.prepareStatement("SELECT * FROM ads WHERE title = ?");
+//            ResultSet rs = stmt.executeQuery();
+//            return createAdsFromResults(rs);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error retrieving all ads.", e);
+//        }
+//    }
