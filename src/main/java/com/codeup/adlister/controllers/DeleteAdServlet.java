862ecc0d7e7429137ebca2f5af5b646b9c97443f
emmadejong <dejong.l.emma@gmail.com>
2018-04-12 10:47:07 -0500

    create the delete ads servlet

---
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
@@ -0,0 +1,22 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteAdServlet", urlPatterns = "/delete/ad")
+public class DeleteAdServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));
+
+        Ad adsDao = (Ad) DaoFactory.getAdsDao();
+//        adsDao.
+    }
+}
