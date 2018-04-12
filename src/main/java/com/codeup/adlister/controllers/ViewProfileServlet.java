c7f953972f5c566e60a8a7031826ee0d8052d662
emmadejong <dejong.l.emma@gmail.com>
2018-04-11 22:32:57 -0500

    working on view users ads in profile

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,6 +1,10 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.Ads;
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.Users;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
@@ -17,6 +22,18 @@ public class ViewProfileServlet extends HttpServlet {
             return;
         }
         request.setAttribute("ads", DaoFactory.getAdsDao().allByUsername());
+//        WORK IN PROGRESS
+//        Users userDao = DaoFactory.getUsersDao();
+//        Ads adsDao = DaoFactory.getAdsDao();
+//
+//        String id = request.getParameter("id");
+//        User user = userDao.findByUserId(Long.parseLong(id));
+//
+//        List<Ad> ads = adsDao.findAdsByUserId(id);
+//
+//        request.setAttribute("user", user);
+//        request.setAttribute("ads", ads);
+
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
