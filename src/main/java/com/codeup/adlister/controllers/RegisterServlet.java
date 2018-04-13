62d41cf820e61386c5db4150e0f4c9e24b753bb5
emmadejong <dejong.l.emma@gmail.com>
2018-04-13 14:51:41 -0500

    added doget method on the send redirect

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -4,14 +4,12 @@ import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.dao.Users;
 import com.codeup.adlister.models.User;
 import com.codeup.adlister.util.Password;
-import com.codeup.adlister.dao.MySQLUsersDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +21,7 @@ public class RegisterServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String first_name = request.getParameter("first_name");
         String last_name = request.getParameter("last_name");
         String username = request.getParameter("username");
@@ -32,7 +30,7 @@ public class RegisterServlet extends HttpServlet {
         String passwordConfirmation = request.getParameter("confirm_password");
 
        Users usersDao = DaoFactory.getUsersDao();
-//        // create and save a new user
+// create and save a new user
         String hashpass = Password.hash(password);
         User user = new User(first_name, last_name, email, username, hashpass);
 
@@ -40,26 +38,25 @@ public class RegisterServlet extends HttpServlet {
         List<String> errors = new ArrayList<>();
 
 // Next we'll have a handful of if statements to check all of our validation constraints
-
         if (DaoFactory.getUsersDao().findByUsername(username)!=null) {
-            errors.add("That username is already taken.");
+            errors.add("That username is already taken!");
         }
 
         boolean passwordsMatch = request.getParameter("password").equals(request.getParameter("confirm_password"));
         if (! passwordsMatch) {
-            errors.add("Password and confirm password must match");
+            errors.add("Password and confirm password must match!");
         }
 
         if (user.getUsername().length() == 0) {
-            errors.add("You must enter a password");
+            errors.add("You must enter a username!");
         }
 
         if (user.getEmail().length() == 0 || !user.getEmail().contains("@")) {
-            errors.add("Please enter a valid email address");
+            errors.add("Please enter a valid email address!");
         }
 
         if (user.getPassword().length() == 0) {
-            errors.add("You must choose a password");
+            errors.add("You must choose a password!");
         }
 
 // once we're done with all the validation checking...
@@ -71,7 +68,7 @@ public class RegisterServlet extends HttpServlet {
         } else {
             // we have at least one validation error, go back to the register form
             request.getSession().setAttribute("errors", errors);
-            response.sendRedirect("/register");
+            doGet(request, response);
         }
     }
 }
